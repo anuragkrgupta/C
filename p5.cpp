50fc8dc5a5892ebc79dad3899565c034ae021b5a
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2024-06-27 01:13:23 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,11 @@
+//character array intro
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[15];
+    char city[15];
+    cout<<"Enter name and city";
+    cin>>name>>city;
+    cout<<"Hello "<<name<<" your city is "<<city;
+}
---
+++ p6.cpp
@@ -0,0 +1,11 @@
+//expression buiding
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,r;
+    cout<<"Enter a & b";
+    cin>>a>>b;
+    r=(a+b)*(a+b)*(a+b);
+    cout<<"Result ="<<r;
+}
