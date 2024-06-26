b48e65759bbf56e1b68e12ca87eea61ca3a4fe57
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2024-06-27 01:11:51 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    char L,O,W,E,R;
+    printf("Enter character: ");
+    scanf("%c",&L);
+    printf("Enter character: ");
+    scanf("%c",&O);
+    printf("Enter character: ");
+    scanf("%c",&W);
+    printf("Enter character: ");
+    scanf("%c",&E);
+    printf("Enter character: ");
+    scanf("%c",&R);
+    printf("%d ",L);
+    printf("%d ",O);
+    printf("%d ",W);
+    printf("%d ",E);
+    printf("%d ",R);
+
+}
